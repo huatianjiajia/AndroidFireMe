d68dbdd4c86d660e64801639050c0de63678d12e
wxw <3336370748@qq.com>
2017-06-13 11:37:57 +0800

    1.初始提交；

---
+++ app/src/main/java/com/jaydenxiao/androidfire/ui/main/activity/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.jaydenxiao.androidfire.ui.main.activity;
+
+import com.jaydenxiao.androidfire.R;
+import com.jaydenxiao.common.base.BaseActivity;
+
+
+/**
+ * 项目名称： AndroidFireWxw
+ * 包名：com.jaydenxiao.androidfire.ui.main.activity
+ * 类描述： describe
+ * 创建人： wxw https://github.com/huatianjiajia
+ * 创建时间： 2017/6/13 11:25
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.act_splash;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
